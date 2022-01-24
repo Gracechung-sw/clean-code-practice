dee77cd88f903dea1f2a5771c9413c8dc41bae04
Gracechung-sw <hjngy0511@gmail.com>
2022-01-25 07:24:18 +0900

    Add javadoc & Nullable sign: As Clean Code Nullable같은 경우는 코드 하면서 중간중간에 null 값이 될 수 있다는 것을 표시하는 용도로 사용하는게 좋다.

--- src/main/java/com/practice/houseutils/policy/BrokerageRule.java
+++ src/main/java/com/practice/houseutils/policy/BrokerageRule.java
@@ -1,10 +1,16 @@
 package com.practice.houseutils.policy;
 
 import lombok.AllArgsConstructor;
+import org.springframework.lang.Nullable;
 
+/**
+ * 가격이 특정 범위일 때 상한효율과 상한금액을 가지는 클래스
+ */
 @AllArgsConstructor
 public class BrokerageRule {
     private Double brokeragePercent;
+
+    @Nullable
     private Long limitAmount;
 
     public Long calcMaxBrokerage(Long price) {
