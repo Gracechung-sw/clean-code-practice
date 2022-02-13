9e7e32004b9a501a14ee6e5e92a16499d3c7acf6
Gracechung-sw <hjngy0511@gmail.com>
2022-02-13 15:16:06 +0900

    Add calculate apartment brokerage price using DataStructure

--- src/main/java/com/practice/houseutils/entity/Apartment.java
+++ src/main/java/com/practice/houseutils/entity/Apartment.java
@@ -1,2 +1,26 @@
-package com.practice.houseutils.entity;public class Apartment {
+package com.practice.houseutils.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Apartment {
+
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    public String name;
+
+    @Column(nullable = false)
+    public String address;
+
+    @Column(nullable = false)
+    public Long price;
+
 }
--- src/main/java/com/practice/houseutils/service/ApartmentService.java
+++ src/main/java/com/practice/houseutils/service/ApartmentService.java
@@ -1,10 +1,21 @@
 package com.practice.houseutils.service;
 
+import com.practice.houseutils.HouseutilsApplication;
 import com.practice.houseutils.repository.ApartmentRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-public class ApartmetnService {
+import javax.transaction.Transactional;
 
-    @Service
-    private final ApartmentRepository apartmentRepository;// 생성자 주입
+@Service
+@AllArgsConstructor // 생성자 주입
+public class ApartmentService {
+    private final ApartmentRepository apartmentRepository;
+
+    @Transactional
+    public Long getPriceOrThrow(Long apartmentId) {
+        return apartmentRepository.findById(apartmentId)
+                .orElseThrow(() -> new HouseutilsApplication(ErrorCode.ENTITY_NOT_FOUND))
+                .getPrice();
+    }
 }
