cafb9ca58b1344261d1b09482a3618df663dd498
Gracechung-sw <hjngy0511@gmail.com>
2022-02-13 15:37:39 +0900

    Change to use ErrorHandling class

--- src/main/java/com/practice/houseutils/policy/BrokerPolicyFactory.java
+++ src/main/java/com/practice/houseutils/policy/BrokerPolicyFactory.java
@@ -1,6 +1,8 @@
 package com.practice.houseutils.policy;
 
 import com.practice.houseutils.constants.ActionType;
+import com.practice.houseutils.exception.ErrorCode;
+import com.practice.houseutils.exception.HouseUtilsException;
 
 public class BrokerPolicyFactory {
     public static BrokeragePolicy of(ActionType actionType) {
@@ -10,7 +12,7 @@ public class BrokerPolicyFactory {
             case PURCHASE:
                 return new PurchaseBrokeragePolicy();
             default:
-                throw new IllegalArgumentException("해당 actionType에 대한 정책이 존재하지 않습니다.");
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actionType에 대한 정책이 존재하지 않습니다. ");
         }
     }
 }
