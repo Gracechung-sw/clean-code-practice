d116a874d5e2937642f4917ba380adbf871d32b7
Grace chung <55108539+Gracechung-sw@users.noreply.github.com>
2022-02-13 15:17:40 +0900

    Chapter6 object data structure (#3)

---
+++ src/main/java/com/practice/houseutils/repository/ApartmentRepository.java
@@ -0,0 +1,8 @@
+package com.practice.houseutils.repository;
+
+import com.practice.houseutils.entity.Apartment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
+
+}
